97671ee1b6e7ff682f239fb06f6cc77ee8dc5ffb
sanchez005909 <sanchez005909@gmail.com>
2023-08-05 14:04:42 +0300

    classmethod and staticmethod

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+import math
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -12,10 +15,41 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.getter
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            self.__name = name[:10]
+            return self.__name
+        self.__name = name
+        return self.__name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv', newline='', encoding='windows-1251') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                name, price, quantity = row
+                if price.isalpha():
+                    continue
+                emp = cls(name, price, quantity)
+                cls.all.append(emp)
+
+    @staticmethod
+    def string_to_number(str_num):
+        return math.floor(float(str_num))
+
 
     def calculate_total_price(self) -> float:
         """
