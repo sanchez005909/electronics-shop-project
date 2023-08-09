8be2aa03fd5b06e4678c4559e2ff783250d9f099
sanchez005909 <sanchez005909@gmail.com>
2023-08-09 16:42:46 +0300

    add __str__ and __repr__

--- src/config.py
+++ src/config.py
@@ -1,7 +1,5 @@
-import pathlib
-from pathlib import Path
+import os.path
+import os
 
-# Получаем строку, содержащую путь к рабочей директории:
-dir_path = pathlib.Path.cwd()
-# Объединяем полученную строку с недостающими частями пути
-path = Path(dir_path, 'src', 'items.csv')
+a = os.path.abspath('../src')
+path = os.path.join(a, 'items.csv')
